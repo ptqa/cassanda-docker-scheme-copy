bedf3fdaad82eba0c482011241ae24693c1047a7
Tony Nyurkin <anyurkin@kaaiot.io>
2018-05-10 19:07:36 +0300

    Fixing

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,82 +1,82 @@
 #!/bin/bash
-set -e
 
-# first arg is `-f` or `--some-option`
-# or there are no args
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	set -- cassandra -f "$@"
+# This EnTrypoinT is more or less The same as The EnTrypoinT from The official image: changes 68-73!
+
+if [ "${1:0:1}" = '-' ]; then
+  set -- cassandra -f "$@"
 fi
 
-# allow the container to be started with `--user`
 if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
-	chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
-	exec gosu cassandra "$BASH_SOURCE" "$@"
-fi
 
-_ip_address() {
-	# scrape the first non-localhost IP address of the container
-	# in Swarm Mode, we often get two IPs -- the container IP, and the (shared) VIP, and the container IP should always be first
-	ip address | awk '
-		$1 == "inet" && $2 !~ /^127[.]/ {
-			gsub(/\/.+$/, "", $2)
-			print $2
-			exit
-		}
-	'
-}
+  chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
+  exec gosu cassandra "$BASH_SOURCE" "$@"
+fi
 
 if [ "$1" = 'cassandra' ]; then
-	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
+  : ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
 
-	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
-	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
-		CASSANDRA_LISTEN_ADDRESS="$(_ip_address)"
-	fi
-
-	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
-
-	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
-		CASSANDRA_BROADCAST_ADDRESS="$(_ip_address)"
-	fi
-	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
-
-	if [ -n "${CASSANDRA_NAME:+1}" ]; then
-		: ${CASSANDRA_SEEDS:="cassandra"}
-	fi
-	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
-	
-	sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
-
-	for yaml in \
-		broadcast_address \
-		broadcast_rpc_address \
-		cluster_name \
-		endpoint_snitch \
-		listen_address \
-		num_tokens \
-		rpc_address \
-		start_rpc \
-	; do
-		var="CASSANDRA_${yaml^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
-		fi
-	done
-
-	for rackdc in dc rack; do
-		var="CASSANDRA_${rackdc^^}"
-		val="${!var}"
-		if [ "$val" ]; then
-			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
-		fi
-	done
-
-  if [ -n "${COPY_SCHEME_FROM}" ]; then
-    cqlsh $COPY_SCHEME_FROM -e "DESCRIBE KEYSPACE $COPY_SCHEME_KEYSPACE" > init_schema.cql
-    cqlsh -e "SOURCE 'init_schema.cql'" 1>&2 > /dev/null
+  : ${CASSANDRA_LISTEN_ADDRESS='auto'}
+  
+  if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
+    CASSANDRA_LISTEN_ADDRESS="$(hostname --ip-address)"
   fi
+
+  : ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
+
+  if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
+    CASSANDRA_BROADCAST_ADDRESS="$(hostname --ip-address)"
+  fi
+  
+  : ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
+
+  if [ -n "${CASSANDRA_NAME:+1}" ]; then
+    : ${CASSANDRA_SEEDS:="cassandra"}
+  fi
+  
+  : ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
+  
+  sed -ri 's/(- seeds:).*/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+
+  for yaml in \
+    broadcast_rpc_address \
+    broadcast_address \
+    endpoint_snitch \
+    listen_address \
+    cluster_name \
+    rpc_address \
+    num_tokens \
+    start_rpc \
+  ; do
+    var="CASSANDRA_${yaml^^}"
+    val="${!var}"
+    if [ "$val" ]; then
+      sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+    fi
+  done
+
+  for rackdc in dc rack; do
+    var="CASSANDRA_${rackdc^^}"
+    val="${!var}"
+    if [ "$val" ]; then
+      sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+    fi
+  done
 fi
 
+# Works only if you define Tables like: CreaTe if noT exisTs - oTherwise following loop runs infiniTely!
+
+if [ -n "${COPY_SCHEME_FROM}" ]; then
+  cqlsh $COPY_SCHEME_FROM -e "DESCRIBE KEYSPACE $COPY_SCHEME_KEYSPACE" > /tmp/init_schema.cql 2> /dev/null
+  sed -i 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' /tmp/init_schema.cql
+  sed -i 's/CREATE KEYSPACE/CREATE KEYSPACE IF NOT EXISTS/g' /tmp/init_schema.cql
+  sed -i '/Warning/d' /tmp/init_schema.cql
+fi
+
+for f in /tmp/*; do
+  case "$f" in
+    *.cql) echo "$0: running $f" && until cqlsh -f "$f"; do >&2 echo "Cassandra is unavailable - sleeping"; sleep 2; done & ;;
+  esac
+  echo
+done
 
 exec "$@"
